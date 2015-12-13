67c37c9a86fa06e139e85af3935bb25e1293c80b
Richard Terry <git@radiac.net>
2015-12-13 09:53:31 +0000

    Roll back change to angel start delay

--- mara/angel.py
+++ mara/angel.py
@@ -153,9 +153,6 @@ class Angel(object):
                     socket_to_process[process_socket] = active_process
                     process_to_socket[active_process] = process_socket
                     
-                    # Reset the delay
-                    start_delay = 0
-                
                 # Check process sockets who want to say something
                 for process_socket in read_sockets:
                     process = socket_to_process[process_socket]
@@ -212,6 +209,9 @@ class Angel(object):
                     elif cmd == CMD_STARTED:
                         # New process has started, how nice for it
                         process_socket.send((CMD_OK, None))
+                        
+                        # It's not going to fall over immediately, reset delay
+                        start_delay = 0
                         
                         # Tell old process it's ok to die now
                         if old_process in process_to_socket:
